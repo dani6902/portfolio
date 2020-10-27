debdbbb609b225289a7b02e006fc3409328d3cb9
dani6902 <60360349+dani6902@users.noreply.github.com>
2020-10-27 03:11:40 +0100

    header og frontside

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,4 +1,14 @@
 console.log("JS is running");
+// ligger baggrund på header
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    }else{
+      $('.navbar').removeClass("sticky");
+    }
+  })
+});
 
 // henter knappen der åbner modal
 var btn = document.querySelectorAll("button.modal-button");
