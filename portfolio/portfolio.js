3153932fb36e1aa20c59e58354a0c341e72b9702
dani6902 <60360349+dani6902@users.noreply.github.com>
2020-11-03 23:47:01 +0100

    projekter og kontakside

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -8,40 +8,26 @@ $(document).ready(function(){
       $('.navbar').removeClass("sticky");
     }
   })
+  // owl carousel script
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    resposive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items: 2,
+        nav: false
+      },
+      1000:{
+        items: 3,
+        nav: false
+      }
+    }
+  });
+
 });
-
-// henter knappen der åbner modal
-var btn = document.querySelectorAll("button.modal-button");
-
-// Alle sider moddals
-var modals = document.querySelectorAll('.modal');
-
-// Henter <span> element der lukker modal.
-var spans = document.getElementsByClassName("close");
-
-// Når brugeren klikker på button åbner modal
-for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("data-href"));
-    modal.style.display = "block";
- }
-}
-
-// Når brugeren klikker på <span> (x), lukker modal
-for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-    }
- }
-}
-
-// Når brugeren klikker alle andre steder lukker modal
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-     }
-    }
-}
